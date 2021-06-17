0fc050691b3f18eb6b343b19830e38c1b94805f8
NATHEN-PC\nathe <whit1023@algonquinlive.com>
2021-06-17 15:40:45 -0400

    Added SSL verification, allowing usage between the backend Django server and the React frontend.

--- DashboardBackend/settings.py
+++ DashboardBackend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'dashboard.apps.DashboardConfig',
+    'corsheaders',
 ]
 
 REST_FRAMEWORK = {
@@ -58,7 +59,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'DashboardBackend.urls'
 
